4a11c2c41a6a15bf07cf5b12b9568c88881fc160
ass <assanthiam247@gmail.com>
2026-06-17 18:00:54 +0200

    feat: ajout du RapportController pour l'export Excel des visites

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,6 +10,8 @@ Route::get('/', function () {
 
 Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/visites/export', [\App\Http\Controllers\RapportController::class, 'exportVisites'])->middleware(['auth'])->name('visites.export');
+
 Route::get('/download-pdf', \App\Http\Controllers\PdfController::class)->middleware(['auth'])->name('pdf.download');
 Route::get('/rapports', [\App\Http\Controllers\RapportController::class, 'index'])->middleware(['auth'])->name('rapports.index');
 
@@ -23,5 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::resource('visites', \App\Http\Controllers\VisiteController::class);
 
 });
+
 
 require __DIR__.'/auth.php';
